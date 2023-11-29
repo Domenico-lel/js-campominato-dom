5104f768c10eb0d48334a970167ff4fc7c3002ba
Domenico-lel <domenicolella2@gmail.com>
2023-11-29 12:00:11 +0100

    creato boott per num pari e disp (non funzionante)

--- boot.js
+++ boot.js
@@ -11,8 +11,23 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 const gridElement = document.getElementById("grid");
 const startButton = document.getElementById("startButton");
 
-for (let i = 1; i <= 64; i++) {
+// generare un array di numeri in ordine casuale in un range
+const newArrNum = genArrayRandomNum(1, 64, 64);
+
+for (let i = 1; i <= newArrNum.length; i++) {
     const newElement = createMyElement("div", "square");
+
+    // inseriamo il numero all'interno della cella
+    let numeroCellaIesimo = newArrNum[i];
+    newElement.append(numeroCellaIesimo);
+
+    // a seconda che numeroiesimo sia pari o dispari,
+    // inserirò una classe diversa
+    if (numeroCellaIesimo % 2 === 0) {
+        newElement.classList.add("square-even");
+    } else {
+        newElement.classList.add("square-odd");
+    }
 
     // Aggiungi il numero come testo all'interno del div
     newElement.textContent = i;
@@ -36,8 +51,34 @@ startButton.addEventListener("click",
 
 // definizione funzione 
 function createMyElement(tagtype, classname) {
+
     const currentElement = document.createElement(tagtype);
     currentElement.classList.add(classname);
+    
     return currentElement;
 }
 
+
+
+// funzione che crea un array con ordinamento randomico di numeri in un range (min, max)
+function genArrayRandomNum(minNum, maxNum, lunghezzaArr) {
+    // array da popolare e poi tornare 
+    const arrayToGen = [];
+
+    // ciclo che mi popolerà l'array
+    while (arrayToGen.length < lunghezzaArr) {
+        // generare un numero random in un range (min, max)
+        let newNumber = genRandomNumMinMax(minNum, maxNum);
+        // se il numero generato NON è gia presente nell'array
+        if (!arrayToGen.includes(newNumber)) {
+            // allora lo pusho nell'array
+            arrayToGen.push(newNumber);
+        }
+    }
+    return arrayToGen;
+}
+
+// funzione che genera un numero random in un rantge (min, max)
+function genRandomNumMinMax(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
